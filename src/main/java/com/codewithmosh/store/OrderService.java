2946b6aac22f914ef44965aac3afeef22b90d73d
IanD1013 <ian.tung1013@gmail.com>
2025-03-07 10:24:00 +1300

    Bean lifecycle hooks

--- src/main/java/com/codewithmosh/store/OrderService.java
+++ src/main/java/com/codewithmosh/store/OrderService.java
@@ -5,12 +5,26 @@ package com.codewithmosh.store;
 //import org.springframework.stereotype.Component;
 //import org.springframework.stereotype.Service;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 //@Service
 public class OrderService {
     private PaymentService paymentService;
 
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
+        System.out.println("OrderService created");
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("OrderService PostConstruct.");
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        System.out.println("OrderService PreDestroy.");
     }
 
     public void placeOrder() {
