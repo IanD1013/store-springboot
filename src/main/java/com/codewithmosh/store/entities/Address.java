73891d46e106c9618d41fb964602bf38f5587699
IanD1013 <ian.tung1013@gmail.com>
2025-03-08 09:58:22 +1300

    Define one-to-many relationships

--- src/main/java/com/codewithmosh/store/entities/Address.java
+++ src/main/java/com/codewithmosh/store/entities/Address.java
@@ -1,9 +1,12 @@
 package com.codewithmosh.store.entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Getter
 @Setter
 @Entity
@@ -22,4 +25,9 @@ public class Address {
 
     @Column(name = "zip")
     private String zip;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @ToString.Exclude
+    private User user;
 }
