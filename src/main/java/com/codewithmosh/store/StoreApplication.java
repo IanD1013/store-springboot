5831a875cab9f3ac21c0f0e97d26029aab480c24
IanD1013 <ian.tung1013@gmail.com>
2025-03-08 22:34:43 +1300

    Manage transactions

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -1,7 +1,6 @@
 package com.codewithmosh.store;
 
-import com.codewithmosh.store.entities.User;
-import com.codewithmosh.store.repositories.UserRepository;
+import com.codewithmosh.store.services.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,21 +9,7 @@ import org.springframework.context.ApplicationContext;
 public class StoreApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-        var repository = context.getBean(UserRepository.class);
-
-//        var user = User.builder()
-//                .name("John")
-//                .email("john@example.com")
-//                .password("password")
-//                .build();
-//
-//        repository.save(user);
-
-//        var user = repository.findById(1L).orElseThrow();
-//        System.out.println(user.getEmail());
-
-        repository.findAll().forEach(u -> System.out.println(u.getEmail()));
-
-        repository.deleteById(1L);
+        var service = context.getBean(UserService.class);
+        service.showEntityStates();
     }
 }
