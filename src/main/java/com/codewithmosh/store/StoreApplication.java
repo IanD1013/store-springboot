edb22cc10dace11cf1b1de3c36ec2fe9eea15343
IanD1013 <ian.tung1013@gmail.com>
2025-03-08 21:19:29 +1300

    Use repositories

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -1,5 +1,7 @@
 package com.codewithmosh.store;
 
+import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,5 +10,21 @@ import org.springframework.context.ApplicationContext;
 public class StoreApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        var repository = context.getBean(UserRepository.class);
+
+//        var user = User.builder()
+//                .name("John")
+//                .email("john@example.com")
+//                .password("password")
+//                .build();
+//
+//        repository.save(user);
+
+//        var user = repository.findById(1L).orElseThrow();
+//        System.out.println(user.getEmail());
+
+        repository.findAll().forEach(u -> System.out.println(u.getEmail()));
+
+        repository.deleteById(1L);
     }
 }
