1d4bf2a043bfae1ea5dfac10a5e393b43735e292
IanD1013 <ian.tung1013@gmail.com>
2025-03-09 22:25:16 +1300

    Write custom queries using @Query

--- src/main/java/com/codewithmosh/store/repositories/ProductRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/ProductRepository.java
@@ -1,38 +1,26 @@
 package com.codewithmosh.store.repositories;
 
 import com.codewithmosh.store.entities.Product;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
-    // String
-    List<Product> findByName(String name);
-    List<Product> findByNameLike(String name);
-    List<Product> findByNameNotLike(String name);
-    List<Product> findByNameContaining(String name);
-    List<Product> findByNameStartingWith(String name);
-    List<Product> findByNameEndingWith(String name);
-    List<Product> findByNameEndingWithIgnoreCase(String name);
+    // Find products whose prices are in a given range and sort them by name
+    // SQL or JPQL
+    @Query(value = "select * from products p where p.price between :min and :max order by p.name", nativeQuery = true)
+    List<Product> findProducts(@Param("min") BigDecimal min, @Param("max") BigDecimal max);
 
-    // Number
-    List<Product> findByPrice(BigDecimal price);
-    List<Product> findByPriceGreaterThan(BigDecimal price);
-    List<Product> findByPriceGreaterThanEqual(BigDecimal price);
-    List<Product> findByPriceBetween(BigDecimal min, BigDecimal max);
+    // Aggregate
+    @Query("select count(*) from Product p where p.price between :min and :max")
+    List<Product> countProducts(@Param("min") BigDecimal min, @Param("max") BigDecimal max);
 
-    // Null
-    List<Product> findByDescriptionNull();
-    List<Product> findByDescriptionNotNull();
-
-    // Multiple conditions
-    List<Product> findByDescriptionNullAndNameNull();
-
-    // Sort (OrderBy)
-    List<Product> findByNameOrderByNameAsc(String name);
-
-    // Limit (Top/First)
-    List<Product> findTop5ByNameOrderByPrice(String name);
-    List<Product> findFirst5ByNameLikeOrderByPrice(String name);
+    // Update
+    @Modifying
+    @Query("update Product p set p.price = :newPrice where p.category.id = :categoryId")
+    void updatePriceByCategory(BigDecimal newPrice, Byte categoryId);
 }
