97dce816513c1e5de185a06173e80d15a8dedc91
IanD1013 <ian.tung1013@gmail.com>
2025-03-10 09:04:10 +1300

    Fetch partial data with projections

---
+++ src/main/java/com/codewithmosh/store/dtos/ProductSummaryDTO.java
@@ -0,0 +1,11 @@
+package com.codewithmosh.store.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ProductSummaryDTO {
+    private Long id;
+    private String name;
+}
